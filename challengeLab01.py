f371aa9617af84971a8b8da009bc8dcbd8cbe548
Marcelo Nazario <ind7@protonmail.com>
2020-08-10 17:19:55 -0400

    Finished lab 8

---
+++ challengeLab01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+name_input = input("What is your name? ")
+
+dweek_input = input("What day of the week it is? ") 
+
+
+print(f"Hello, {name_input}! Happy {dweek_input}!")
+
+
---
+++ mylunch.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+name = input("Type your name here: ")
+
+print("Hello", name, "!")
+
+print("Let me get lunch early!")
+

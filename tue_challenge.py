2830dea3392a779ef45fa036e9f17d4ba6041064
Marcelo Nazario <ind7@protonmail.com>
2020-08-11 11:58:39 -0400

    learning about dictionaries

---
+++ tue_challenge.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+icecream= ["flavors", "salty"]
+
+icecream.append(99)
+
+user_name= input("What's your name? ")
+
+print(f"{icecream[-1]} {icecream[0]}, and {user_name} chooses to be {icecream[1]}.")
